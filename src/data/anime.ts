b8d1dc6102144fbb24d89d820f64526ef553a80e
Mathias Enzler <mathiasenzlerhp@gmail.com>
2025-02-10 16:24:18 -0300

    Commit actualizando el index, category etc

--- src/data/anime.ts
+++ src/data/anime.ts
@@ -7,44 +7,48 @@ export interface Anime {
     rating: number;
     episodes?: number;
     status: 'Ongoing' | 'Completed' | 'Upcoming';
-    type: 'TV' | 'Movie' | 'OVA';
+    type: 'TV' | 'Hentai' | 'OVA';
     censored: boolean;
+    description: string;
+    trailerUrl?: string; // URL del video de YouTube
+    releaseYear: number;
+    studio: string;
 }
+
 
 export const animeData: Anime[] = [
     {
         id: "overflow",
         title: "Overflow",
         imageUrl: "/img/overflow.jpg",
-        categories: ["Nuevos"],
-        rating: 4.8,
-        episodes: 25,
+        categories: ["Nuevos", "Sin Censura",],
+        rating: 5,
+        episodes: 8,
         status: "Ongoing",
-        type: "TV",
-        censored: false
+        type: "Hentai",
+        censored: false,
+        description: "La trama se centra en la vida de Kazushi Sudou, un joven universitario que vive con sus dos amigas de la infancia, las hermanas Ayane y Kotone Shirakawa. Todo comienza de manera casual cuando las hermanas deciden usar el baño de Kazushi y, a partir de ahí, surgen una serie de situaciones íntimas y comprometedoras que van intensificándose con cada episodio. El anime presenta una historia llena de malentendidos, momentos de comedia y un enfoque en situaciones románticas y sensuales entre los personajes principales. Aunque Overflow contiene contenido explícito, es considerado más ligero en comparación con otras obras hentai, ya que mantiene un tono más relajado y cómico en algunos momentos.",
+        trailerUrl: "https://www.youtube.com/embed/maV5wZXV8Ro?si=cUkUU1F-P2vgMIom",
+        releaseYear: 2020,
+        studio: "Studio Hokiboshi."
     },
     {
-        id: "the-dangers-in-my-heart",
-        title: "The Dangers in My Heart",
-        imageUrl: "/img/anime2.webp",
-        categories: ["Sin Censura"],
-        rating: 4.8,
-        episodes: 25,
-        status: "Ongoing",
-        type: "TV",
-        censored: false
+        id: "isekai-killer",
+        title: "The Man Who Saved Me on My Isekai Trip Was a Killer",
+        imageUrl: "/img/anime3.webp",
+        categories: ["Nuevos", ],
+        rating: 4.5,
+        episodes: 12,
+        status: "Upcoming",
+        type: "Hentai",
+        censored: true,
+        description: "La historia sigue a la protagonista, quien es transportada a otro mundo y es salvada por un hombre misterioso con un oscuro secreto: él es un asesino. A pesar de su naturaleza violenta, este hombre se convierte en su protector en un mundo lleno de peligros. Este anime combina acción, supervivencia, y un vínculo poco convencional en medio de constantes amenazas y decisiones difíciles.",
+        trailerUrl: "https://www.youtube.com/embed/vwAdNdhyfi0?si=Nsa-iwzxpTZprq7X",
+        releaseYear: 2024,
+        studio: "Studio TBD"
     },
-    {
-        id: "attack-on-titan",
-        title: "Attack on Titan",
-        imageUrl: "/img/h3.webp",
-        categories: ["Acción", "Fantasía"],
-        rating: 9.1,
-        episodes: 75,
-        status: "Completed",
-        type: "TV",
-        censored: false
-    },
+
+    // Agrega más animes aquí
 ];
 
 
